8cc122a2f94b49a574de695a07d6fd5d1b417a91
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-02-22 23:51:57 +0100

    Richiesta validazione input

--- js/main.js
+++ js/main.js
@@ -21,37 +21,52 @@ let userCode;
 // Variabile box che scompare
 let trainTicket = document.getElementById('ticket');
 
+// Variabili validazione input
+let nameValidation;
+let distanceValidation;
+
 // Evento genera biglietto
 confirmButton.addEventListener('click',
 function() {
-    // Riassegnazione variabili
-    userName = document.getElementById('user-name').value;
-    userDistance = Number(document.getElementById("user-distance").value);
-    userAge = document.getElementById('user-age').value;
-    ticketPrice = userDistance * 0.21;
-    trainCarNumber = Math.round(Math.random() * 10) + 1;
-    userCode = Math.round(Math.random() * 89999) + 10001;
-    // Modifica DOM
-    document.querySelector('.js-passenger-name').innerHTML = userName;
-    document.getElementById('js-train-car').innerHTML = trainCarNumber;
-    document.getElementById('js-user-code').innerHTML = userCode;
-    // Condizione
-    if (userAge === 'minorenne') {
-        ticketPrice = ticketPrice - (ticketPrice * 0.2);
-        document.getElementById('js-ticket-type').innerHTML = 'Biglietto minorenni';
-    }
 
-    else if (userAge === 'over65') {
-        ticketPrice = ticketPrice - (ticketPrice * 0.4);
-        document.getElementById('js-ticket-type').innerHTML = 'Biglietto silver';
-    }
 
-    // Stampa risultati
-    document.getElementById('js-ticket-price').innerHTML = ticketPrice.toFixed(2);
-    document.getElementById('js-ticket-price').append('€');
-    console.log('Il prezzo del biglietto è',ticketPrice,'€');
-    // Mostra il biglietto
-    trainTicket.classList.remove('hidden');
+    // Assegnazione valore per validazione
+    nameValidation = document.getElementById('user-name').value;
+    distanceValidation = document.getElementById('user-distance').value;
+    // Condizione validazione input
+    if (nameValidation === '' || distanceValidation === '') {
+        alert('Riempire i campi richiesti');
+}   else {  
+        // Riassegnazione variabili
+        userName = document.getElementById('user-name').value;
+        userDistance = Number(document.getElementById("user-distance").value);
+        userAge = document.getElementById('user-age').value;
+        ticketPrice = userDistance * 0.21;
+        trainCarNumber = Math.round(Math.random() * 10) + 1;
+        userCode = Math.round(Math.random() * 89999) + 10001;
+        // Modifica DOM
+        document.querySelector('.js-passenger-name').innerHTML = userName;
+        document.getElementById('js-train-car').innerHTML = trainCarNumber;
+        document.getElementById('js-user-code').innerHTML = userCode;
+        // Condizione
+        if (userAge === 'minorenne') {
+            ticketPrice = ticketPrice - (ticketPrice * 0.2);
+            document.getElementById('js-ticket-type').innerHTML = 'Biglietto minorenni';
+        }
+
+        else if (userAge === 'over65') {
+            ticketPrice = ticketPrice - (ticketPrice * 0.4);
+            document.getElementById('js-ticket-type').innerHTML = 'Biglietto silver';
+        }
+
+        // Stampa risultati
+        document.getElementById('js-ticket-price').innerHTML = ticketPrice.toFixed(2);
+        document.getElementById('js-ticket-price').append('€');
+        console.log('Il prezzo del biglietto è',ticketPrice,'€');
+        // Mostra il biglietto
+        trainTicket.classList.remove('hidden');
+    }
+    
 }
 );
 
